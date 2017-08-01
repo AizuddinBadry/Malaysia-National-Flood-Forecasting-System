7892d091c39db8261de37380634686456c0d2fdc
Aizuddin Badry <aizuddinbadry@lucretech.com>
2017-08-01 14:59:29 +0800

    make it better

---
+++ src/Main.js
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import './App.css';
+import $ from "jquery";
+import {Line, Bar} from 'react-chartjs-2';
+import topojson from 'topojson';
+var MapChoropleth = require('react-d3-map-choropleth').MapChoropleth;
+
+
+
+class Main extends Component {
+    
+    constructor() {
+        super();
+        var today = new Date(),
+        DateComponent =  today.getDate() + '-' +(today.getMonth() + 1) + '-' +   today.getFullYear();
+
+        this.state = {
+            DateComponent: DateComponent, current_status:[],
+            oneAM: '',twoAM: '',threeAM: '',fourAM: '',fiveAM: '',sixAM: '',sevenAM: '',eightAM: '',nineAM: '',tenAM: '',elevenAM: '',
+            twelvePM: '', onePM: '',twoPM: '',threePM: '',fourPM: '',fivePM: '',sixPM: '',sevenPM: '',eightPM: '',ninePM: '',tenPM: '',
+            elevenPM: '',twelveAM: '',forecast_oneAM: '',forecast_twoAM: '',forecast_threeAM: '',forecast_fourAM: '',forecast_fiveAM: '',forecast_sixAM: '',forecast_sevenAM: '',forecast_eightAM: '',forecast_nineAM: '',forecast_tenAM: '',forecast_elevenAM: '',
+            forecast_twelvePM: '', forecast_onePM: '',forecast_twoPM: '',forecast_threePM: '',forecast_fourPM: '',forecast_fivePM: '',forecast_sixPM: '',forecast_sevenPM: '',forecast_eightPM: '',forecast_ninePM: '',forecast_tenPM: '',
+            forecast_elevenPM: '',forecast_twelveAM: '',
+        };
+    }
+
+    
+
+  componentDidMount() {
+    var self = this;
+    var water_level = []
+    var forecasted = []
+            $.ajax({
+                    url: "https://banjirv2.herokuapp.com/Kelantan?station_name=Sg.Kelantan di Kuala Krai&date=7-12-2017",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(res) {
+                        for(var i=0;i<res.length;i++) {
+                        water_level.push(+res[i]['water_level']);
+                        forecasted.push(+res[i]['forcasted']);
+                      };
+                        console.log(res);
+                        self.setState({oneAM:water_level[0],twoAM:water_level[1],threeAM: water_level[2],fourAM: water_level[3],fiveAM: water_level[4],sixAM: water_level[5],sevenAM: water_level[6],eightAM: water_level[7],nineAM: water_level[8],tenAM: water_level[9],elevenAM: water_level[10],
+                           twelvePM: water_level[11], onePM: water_level[12],twoPM: water_level[13],threePM: water_level[14],fourPM: water_level[15],fivePM: water_level[16],sixPM: water_level[17],sevenPM: water_level[18],eightPM: water_level[19],ninePM: water_level[20],tenPM: water_level[21], 
+                            elevenPM: water_level[22],twelveAM: water_level[23], current_status:res, forecast_oneAM:forecasted[0],forecast_twoAM:forecasted[1],forecast_threeAM: forecasted[2],forecast_fourAM: forecasted[3],forecast_fiveAM: forecasted[4],forecast_sixAM: forecasted[5],forecast_sevenAM: forecasted[6],forecast_eightAM: forecasted[7],forecast_nineAM: forecasted[8],forecast_tenAM: forecasted[9],forecast_elevenAM: forecasted[10],
+                           forecast_twelvePM: forecasted[11], forecast_onePM: forecasted[12],forecast_twoPM: forecasted[13],forecast_threePM: forecasted[14],forecast_fourPM: forecasted[15],forecast_fivePM: forecasted[16],forecast_sixPM: forecasted[17],forecast_sevenPM: forecasted[18],forecast_eightPM: forecasted[19],forecast_ninePM: forecasted[20],forecast_tenPM: forecasted[21], 
+                            forecast_elevenPM: forecasted[22],forecast_twelveAM: forecasted[23],
+                        });
+                       
+                    },
+                  
+                });
+    }
+
+  render() {
+    var self = this;
+    var bgColors = { "Default": "#81b71a",
+                    "Blue": "#00B1E1",
+                    "Orange": "#F9B10A",
+                    "Green": "#8CC152",
+                    "Red": "#F70000",
+                    "Yellow": "#FEFE0E",
+  };
+     let current_status = self.state.current_status.map((index) =>
+        <tr>
+        <td className="center">{index.time}</td>
+        <td className="center">{index.water_level}</td>
+        <td className="center">{18.37}</td>
+        <td className="center" style={{backgroundColor: bgColors.Green}}>17.00</td>
+        <td className="center" style={{backgroundColor: bgColors.Yellow}}>20.00</td>
+        <td className="center" style={{backgroundColor: bgColors.Orange}}>22.50</td>
+        </tr>
+    );
+    return (
+      <div className="main-content">
+      <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="page-header">
+                            <center>
+                              <h3>Water Level Forecast</h3><br/>
+                              <span className="label label-info"> {this.state.DateComponent}</span>
+                            </center>
+
+                        </div>
+                    </div>
+                </div>
+            
+                <div className="row">
+               
+                    <div className="col-sm-12">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <i className="clip-stats"></i> Kelantan Station Water Level Forecast
+                                <div className="panel-tools">
+                                    <a className="btn btn-xs btn-link panel-collapse collapses">
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="panel-body">
+                            <div className="row">
+                              <div className="col-md-12">
+                                <div className="panel panel-default">
+                                    
+                                        <table className="table table-bordered table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th className="center">Station Name</th>
+                                                    <th className="center">Last Forecast (m)</th>
+                                                     <th className="center">+6 Hours (m)</th>
+                                                    <th className="center">+12 Hours (m)</th>
+                                                    <th className="center">+24 Hours (m)</th>
+                                                    <th className="center">+48 Hours (m)</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                 <tr>
+                                                  <td className="center">Sg. Lebir di Kuala Koh</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.50</td>
+                                                  </tr>
+                                                  <tr>
+                                                  <td className="center">Sg. Galas di Limau Kasturi</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>7.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>7.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>7.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Orange}}>8.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>7.50</td>
+                                                  </tr>
+                                                  <tr>
+                                                  <td className="center">Sg. Lebir di Dabong</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>15.22</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>15.17</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>15.70</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>15.88</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>15.71</td>
+                                                  </tr>
+                                                   <tr>
+                                                  <td className="center">Sg. Lebir di Dabong</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>18.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Orange}}>22.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Yellow}}>21.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Orange}}>22.50</td>
+                                                  </tr>
+                                                   <tr>
+                                                  <td className="center">Sg. Lebir di Tualang</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Red}}>14.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>11.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>11.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Yellow}}>12.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>11.50</td>
+                                                  </tr>
+                                                   <tr>
+                                                  <td className="center">Sg. Kelantan di Kuala Krai</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.50</td>
+                                                  </tr>
+                                                   <tr>
+                                                  <td className="center">Sg. Kelantan di Kusial</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>16.62</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>16.87</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>16.80</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>16.88</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>16.10</td>
+                                                  </tr>
+                                                   <tr>
+                                                  <td className="center">Sg. Golok di Jenob</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>13.02</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>13.27</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>14.00</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>13.18</td>
+                                                  <td className="center" style={{backgroundColor: bgColors.Green}}>13.50</td>
+                                                  </tr>
+                                            </tbody>
+                                        </table>
+                                    
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                                    <div className="footer clearfix">
+                                      <div className="footer-inner">
+                                         &copy; NaFFWS Kelantan 
+                                      </div>
+                                      <div className="footer-items">
+                                          <span className="go-top"><i className="clip-chevron-up"></i></span>
+                                      </div>
+                                  </div>
+                    </div>
+            </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/Sidebar.js
@@ -0,0 +1,225 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+//Kuala krai
+import Station5521444 from './sg_kelantan_kuala_krai';
+import Station5320443 from './sg_lebir_dabong';
+import Station5222452 from './sg_lebir_tualang';
+
+//Gua Musang
+import Station5020401 from './sg_galas_limau_kasturi';
+import Station4824401 from './sg_lebir_kuala_koh';
+
+//Tanah Merah
+import Station5721442 from './sg_kelantan_kusial';
+import Station5818401 from './sg_golok_jenob';
+
+//Kota Bharu
+import Station6122441 from './sg_kelantan_jeti_kastam';
+import Station6021465 from './sg_kelantan_air_mulih';
+//import Station5020401 from './sg_golok_rantau_panjang';
+import Station5919403 from './empangan_bukit_kwong';
+
+//Pasir Mas
+import Station5824401 from './sg_semerak_pasir_putih';
+
+//Pasir Puteh
+//import Station5020401 from './sg_pagau_air_bol';
+
+//Tumpat
+import Station6120401 from './sg_golok_kuala_jambu';
+
+
+
+import Forecast from './Forecast';
+
+const routes = [
+  { path: '/',
+    exact: true,
+    main: () => <h2>Home</h2>
+  },
+  { path: '/sg_lebir_kuala_koh',
+    main: () => <Station4824401/>
+  },
+  { path: '/sg_galas_limau_kasturi',
+    main: () => <Station5020401/>
+  },
+  { path: '/sg_lebir_dabong',
+    main: () => <Station5320443/>
+  },
+  { path: '/sg_lebir_tualang',
+    main: () => <Station5222452/>
+  },
+  { path: '/sg_kelantan_kuala_krai',
+    main: () => <Station5521444/>
+  },
+   { path: '/sg_kelantan_kusial',
+    main: () => <Station5721442/>
+  },
+   { path: '/sg_golok_jenob',
+    main: () => <Station5818401/>
+  },
+   { path: '/sg_kelantan_jeti_kastam',
+    main: () => <Station6122441/>
+  },
+   { path: '/sg_kelantan_air_mulih',
+    main: () => <Station6021465/>
+  },
+  { path: '/empangan_bukit_kwong',
+    main: () => <Station5919403/>
+  },
+  { path: '/sg_semerak_pasir_putih',
+    main: () => <Station5824401/>
+  },
+  { path: '/sg_golok_kuala_jambu',
+    main: () => <Station6120401/>
+  },
+  { path: '/forecast',
+    main: () => <Forecast/>
+  },
+]
+
+const SidebarExample = () => (
+  <Router>
+
+   <div className="main-container">
+        <div className="sidebar">
+
+            <div className="main-navigation navbar-collapse collapse">
+
+                <ul className="main-navigation-menu">
+                   <li className="">
+                        <a>
+                            <i className="clip-bars"></i>
+                            <span className="title"> Kelantan Banjir Statistic </span><i className="icon-arrow"></i>
+                        </a>
+                        <ul className="sub-menu">
+                            <li>
+                                <a>
+                                    <span className="title"> Gua Musang </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                    <li>
+                                            <Link to="/sg_lebir_kuala_koh">Sg. Lebir di Kuala Koh</Link>
+                                            <Link to="/sg_galas_limau_kasturi">Sg. Galas di Limau Kasturi</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                             <li>
+                                <a>
+                                    <span className="title"> Kuala Krai </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                 <li>
+                                            <Link to="/sg_lebir_dabong">Sg.Lebir di Dabong</Link>
+                                    </li>
+                                    <li>
+                                            <Link to="/sg_lebir_tualang">Sg.Lebir di Tualang</Link>
+                                    </li>
+                                    <li>
+                                            <Link to="/sg_kelantan_kuala_krai">Sg.Kelantan di Kuala Krai</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a>
+                                    <span className="title"> Tanah Merah </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                    <li>
+                                            <Link to="/sg_kelantan_kusial">Sg. Kelantan di Kusial</Link>
+                                    </li>
+                                    <li>
+                                            <Link to="/sg_golok_jenob">Sg. Golok di Jenob</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                             <li>
+                                <a>
+                                    <span className="title"> Kota Bharu </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                    <li>
+                                          <Link to="/sg_kelantan_jeti_kastam">Sg. Kelantan di Jeti Kastam</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a>
+                                    <span className="title"> Pasir Mas </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                    <li>
+                                        <Link to="/sg_kelantan_air_mulih">Sg. Kelantan di Air Mulih</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/sg_golok_rantau_panjang">Sg. Golok di Rantau panjang</Link>
+
+                                    </li>
+                                    <li>
+                                      <Link to="/empangan_bukit_kwong">Empangan Bukit Kwong</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                              <li>
+                                <a>
+                                    <span className="title"> Pasir Puteh </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                    <li>
+                                      <Link to="/sg_semerak_pasir_putih">Sg.Semerak di Pasir Putih</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                             <li>
+                                <a>
+                                    <span className="title"> Jeli </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                <li>
+                                      <Link to="/sg_pagau_air_bol">Sg. Pergau di Air Bol</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                                 <li>
+                                <a>
+                                    <span className="title"> Tumpat </span><i className="icon-arrow"></i>
+                                </a>
+                                <ul className="sub-menu">
+                                 <li>
+                                      <Link to="/sg_golok_kuala_jambu">Sg.Golok di Kuala Jambu</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                        </ul>
+                    </li>
+
+                    <li className="">
+                            <Link to="/forecast"><i className="clip-bars"></i>Water Level Forecast</Link>
+                    </li>
+                </ul>
+
+            </div>
+
+        </div>
+        
+        {routes.map((route, index) => (
+          // Render more <Route>s with the same paths as
+          // above, but different components this time.
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+    </div>
+  </Router>
+)
+
+export default SidebarExample
